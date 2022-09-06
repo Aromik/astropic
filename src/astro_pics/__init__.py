019b9706647e69f781e0ad575045f7cf3db5d8f2
Aromik <57267392+Aromik@users.noreply.github.com>
2022-09-06 23:18:34 +0300

    Delete src/astro_pics directory

--- src/astro_pics/__init__.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-subprocess.run(["pip", "install", "astropy"])
-subprocess.run(["pip", "install", "requests"])
-subprocess.run(['pip', 'install', 'pyquery'])
-subprocess.run(["pip", "install", "pillow"])
-subprocess.run(["pip", "install", "matplotlib"])
-subprocess.run(["pip", "install", "numpy"])
-subprocess.run(["pip", "install", "scipy"])
