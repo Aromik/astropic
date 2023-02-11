d13284026340fb58d5a4c7d6598ff9077c520125
Aromik <57267392+Aromik@users.noreply.github.com>
2023-02-11 20:32:37 +0300

    Package rename

--- src/astro_pics/__init__.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-subprocess.run(["pip", "install", "astropy", "-q"])
-subprocess.run(["pip", "install", "requests", "-q"])
-subprocess.run(['pip', 'install', 'pyquery', "-q"])
-subprocess.run(["pip", "install", "pillow", "-q"])
-subprocess.run(["pip", "install", "matplotlib", "-q"])
-subprocess.run(["pip", "install", "numpy", "-q"])
-subprocess.run(["pip", "install", "scipy", "-q"])
-subprocess.run(["pip", "install", "socket", "-q"])
